b863a8bb8e1b0b41fd6c8c2c319691712cf15d09
Itamar Knafo <37014285+itamadev@users.noreply.github.com>
2024-02-19 01:45:08 +0200

    feat: Adding Agile slides using Slidev  (#10)
    * chore: Update .gitignore to include 'public' directory
    
    * chore: Update description and copyright in languages.en.toml
    
    * chore: Update domain configuration instructions in blog post
    
    * feat: Add new agile slides and assets

---
+++ static/slides/agile/assets/PresenterPrint-CuVVR4LA.js
@@ -0,0 +1,17 @@
+import{d as _,u as d,a as p,c as m,b as u,r as h,e as s,f as t,t as o,g as r,F as f,h as g,n as v,i as x,o as n,j as y,k as b,l as N,m as k,_ as P}from"./index-5YIcIt-X.js";import{N as w}from"./NoteDisplay-BcCl02EI.js";const V={class:"m-4"},L={class:"mb-10"},S={class:"text-4xl font-bold mt-2"},T={class:"opacity-50"},B={class:"text-lg"},C={class:"font-bold flex gap-2"},D={class:"opacity-50"},H=t("div",{class:"flex-auto"},null,-1),j={key:0,class:"border-gray-400/50 mb-8"},z=_({__name:"PresenterPrint",setup(F){d(`
+@page {
+  size: A4;
+  margin-top: 1.5cm;
+  margin-bottom: 1cm;
+}
+* {
+  -webkit-print-color-adjust: exact;
+}
+html,
+html body,
+html #app,
+html #page-root {
+  height: auto;
+  overflow: auto !important;
+}
+`),p({title:`Notes - ${m.title}`});const i=u(()=>h.map(a=>{var l;return(l=a.meta)==null?void 0:l.slide}).filter(a=>a!==void 0&&a.noteHTML!==""));return(a,l)=>(n(),s("div",{id:"page-root",style:v(r(x))},[t("div",V,[t("div",L,[t("h1",S,o(r(m).title),1),t("div",T,o(new Date().toLocaleString()),1)]),(n(!0),s(f,null,g(i.value,(e,c)=>(n(),s("div",{key:c,class:"flex flex-col gap-4 break-inside-avoid-page"},[t("div",null,[t("h2",B,[t("div",C,[t("div",D,o(e==null?void 0:e.no)+"/"+o(r(y)),1),b(" "+o(e==null?void 0:e.title)+" ",1),H])]),N(w,{"note-html":e.noteHTML,class:"max-w-full"},null,8,["note-html"])]),c<i.value.length-1?(n(),s("hr",j)):k("v-if",!0)]))),128))])],4))}}),E=P(z,[["__file","C:/local/agile-presentation/agile/node_modules/@slidev/client/internals/PresenterPrint.vue"]]);export{E as default};
